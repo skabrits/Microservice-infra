92f081cfea03f5a83a35557c725b06a95cd8b3ad
skabrits <seva.kabrits@gmail.com>
2023-04-30 15:49:23 +0300

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
 kubectl create namespace argocd
-kubectl apply -n argocd -f "$SCRIPTPATH/../manifests/argocd.yaml"
+kubectl apply -n argocd -f "/vagrant/manifests/argocd.yaml"
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "ClusterIP"}}'
-kubectl apply -f "$SCRIPTPATH/../manifests/argo-cm.yaml"
+kubectl apply -f "/vagrant/manifests/argo-cm.yaml"
 kubectl -n argocd rollout restart deploy argocd-repo-server
 
-kubectl apply -f "$SCRIPTPATH/../manifests/ingress.yaml"
-kubectl apply -f "$SCRIPTPATH/../manifests/argocd"
+kubectl apply -f "/vagrant/manifests/ingress.yaml"
+kubectl apply -f "/vagrant/manifests/argocd"
